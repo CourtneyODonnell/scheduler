850b0002bff699521131ae11d4f6291e78db8d06
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-09-18 22:14:06 -0300

    built the interviewer list

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import InterviewerListItem from "./InterviewerListItem";
+import "components/InterviewerListItem.scss";
+
+export default function interviewerList(props) {
+  const {interviewers} = props;
+  const interviewersListData = interviewers.map(interviewer => {
+    return <InterviewerListItem key={interviewer.id} name={interviewer.name} avatar={interviewer.avatar} selected={props.interviewer===interviewer.id} setInterviewer={props.setInterviewer} />
+  });
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewersListData}</ul>
+    </section>
+  )
+}
