3365717c119ceba89f2a7d22f712e0c69018dc3e
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-09-18 22:01:38 -0300

    build the interviewer list

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "components/InterviewerListItem.scss";
+import classnames from "classnames";
+
+export default function InterviewerListItem(props) {
+
+  const { name, avatar, selected, setInterviewer } = props;
+
+  const interviewerListItemClass = classnames("interviewers__item", {
+    "interviewers__item--selected": selected,
+  });
+  return(
+    <li className={interviewerListItemClass} selected={selected} onClick={setInterviewer}>
+    <img
+      className="interviewers__item-image"
+      src={avatar}
+      alt={name}
+    />
+    {selected && name}
+  </li>
+  );
+}
