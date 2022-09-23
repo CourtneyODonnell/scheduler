74ba0a7842ca3a725002e05bbce319766b234e86
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-09-23 11:49:09 -0300

    complete static version of the schedule, rendering properly

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,46 @@
 import React, { useState } from "react";
 import "components/Application.scss";
 import DayList from "components/DayList";
+import Appointment from "components/Appointment";
+
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
 
 export default function Application(props) {
 
@@ -25,6 +65,12 @@ export default function Application(props) {
     }
   ];
 
+  // turn object into array for schedule
+  const appointmentsArr = Object.values(appointments).map((appointment) => (
+    <Appointment key={appointment.id} {...appointment} />
+  ))
+  appointmentsArr.push(<Appointment key="last" time="5pm" />);
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -48,7 +94,7 @@ export default function Application(props) {
     />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentsArr}
       </section>
     </main>
   );
