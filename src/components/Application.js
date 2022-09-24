e5ba8a3130c5bfcb9a2f893846e478bc4af4a1bd
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-09-24 17:08:09 -0300

    requesting the days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
@@ -46,30 +48,25 @@ export default function Application(props) {
 
   //useState hook
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
 
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    }
-  ];
+  //useEffect hook
+  useEffect(() => {
+    //axios request here...
+    const dayURL = "http://localhost:8001/api/days";
+    axios.get(dayURL).then(response => {
+      console.log(response.data);
+      setDays([...response.data]);
+    })
+}, []);
 
   // turn object into array for schedule
   const appointmentsArr = Object.values(appointments).map((appointment) => (
     <Appointment key={appointment.id} {...appointment} />
   ))
   appointmentsArr.push(<Appointment key="last" time="5pm" />);
+
+
 
   return (
     <main className="layout">
