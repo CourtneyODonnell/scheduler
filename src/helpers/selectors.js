a86da1b1c41313db7f3f3773c7e11f55bf5e9484
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-09-29 21:04:47 -0300

    transforming data with selectors

---
+++ src/helpers/selectors.js
@@ -0,0 +1,17 @@
+import React from  "react";
+
+export function getAppointmentsForDay(state, day) {
+  let selectedDay = state.days.filter(d => d.name === day)[0];
+  let result = [];
+
+  if (!selectedDay) {
+    return [];
+  }
+  
+  for (const appointment of selectedDay.appointments) {
+    const apptObj = state.appointments[appointment];
+    result.push(apptObj);
+  }
+
+  return result;
+};
