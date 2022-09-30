32238aba9dd03a41c57afdc7a88278775d3ae7a7
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-09-30 15:50:20 -0300

    set up to receive api appointment data

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,44 +5,44 @@ import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
 
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
+// const appointments = {
+//   "1": {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   "2": {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer:{
+//         id: 3,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+//   "3": {
+//     id: 3,
+//     time: "2pm",
+//   },
+//   "4": {
+//     id: 4,
+//     time: "3pm",
+//     interview: {
+//       student: "Archie Andrews",
+//       interviewer:{
+//         id: 4,
+//         name: "Cohana Roy",
+//         avatar: "https://i.imgur.com/FK8V841.jpg",
+//       }
+//     }
+//   },
+//   "5": {
+//     id: 5,
+//     time: "4pm",
+//   }
+// };
 
 export default function Application(props) {
 
@@ -52,6 +52,9 @@ export default function Application(props) {
     days: [],
     appointments: {},
   });
+
+  //daily appointments variable
+  const dailyAppointments = [];
 
   const setDay = day => {
     // console.log(day);
@@ -73,7 +76,7 @@ export default function Application(props) {
 }, []);
 
   // turn object into array for schedule
-  const appointmentsArr = Object.values(appointments).map((appointment) => (
+  const appointmentsArr = Object.values(dailyAppointments).map((appointment) => (
     <Appointment key={appointment.id} {...appointment} />
   ))
   appointmentsArr.push(<Appointment key="last" time="5pm" />);
