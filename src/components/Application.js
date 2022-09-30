59489b76511874645a8b8f6c2a4df97bae6fcac3
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-09-30 15:07:29 -0300

    managing state

--- src/components/Application.js
+++ src/components/Application.js
@@ -47,15 +47,27 @@ const appointments = {
 export default function Application(props) {
 
   //useState hook
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+  });
+
+  const setDay = day => {
+    // console.log(day);
+    return setState({ ...state, day:day })
+  };
+  
+  const setDays = (days) => {
+    setState(prev => ({ ...prev, days }));
+  }
 
   //useEffect hook
   useEffect(() => {
     //axios request here...
     const dayURL = "http://localhost:8001/api/days";
     axios.get(dayURL).then(response => {
-      console.log(response.data);
+      //console.log(response.data);
       setDays([...response.data]);
     })
 }, []);
@@ -79,8 +91,8 @@ export default function Application(props) {
     <hr className="sidebar__separator sidebar--centered" />
     <nav className="sidebar__menu">
       <DayList
-        days={days}
-        value={day}
+        days={state.days}
+        value={state.day}
         onChange={setDay}
     />
     </nav>
