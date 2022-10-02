7026ddf1faae55a76bd969b43ebd1ad31203bd76
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-10-02 20:19:01 -0300

    correct failing tests

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,14 +4,23 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  const transition = (newMode) => {
-    setHistory(prev => [...prev, newMode]);
+  const transition = (newMode, replace = false) => {
+    if (replace) {
+      history.pop();
+      history.push(newMode);
+      setHistory(history);
+    } else {
+      setHistory(prev => [...prev, newMode]);
+    }
     setMode(newMode);
   };
+
   const back = () => {
-    history.pop();
-    setMode(history.slice(-1)[0]);
-    setHistory(history);
+    if (history.length > 1) {
+      history.pop();
+      setMode(history.slice(-1)[0]);
+      setHistory(history);
+    }
   };
 
   return { mode, transition, back };
