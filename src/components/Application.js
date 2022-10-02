966f4424938eb5887c09103de39aeb636acbd5ab
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-10-02 15:50:21 -0300

    retrieving interviewer data not rendering yet

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,10 +1,9 @@
-
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 
 export default function Application(props) {
@@ -15,8 +14,20 @@ export default function Application(props) {
     appointments: {},
   });
 
-  //daily appointments variable
-  const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const appointments = getAppointmentsForDay(state, state.day);
+
+  const schedule = appointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+  
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
+  });
 
   const setDay = day => setState(prev => ({ ...prev, day }));
 
@@ -27,9 +38,15 @@ export default function Application(props) {
       axios.get('/api/appointments'),
       axios.get('/api/interviewers')  
     ]).then((all) => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+      setState(prev => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data
+      }));
     });
 }, []);
+  console.log(state.interviewers);
 
   return (
     <main className="layout">
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -6,3 +6,16 @@ export function getAppointmentsForDay(state, day) {
   
   return selectedDay.appointments.map(appointment => state.appointments[`${appointment}`]);
 };
+
+export function getInterview(state, interview) {
+  return interview
+    ? {
+      student: interview.student,
+      interviewer: {
+        id: interview.interviewer,
+        name: state.interviewers[interview.interviewer].name,
+        avatar: state.interviewers[interview.interviewer].avatar
+      }
+    }
+    : null;
+};
