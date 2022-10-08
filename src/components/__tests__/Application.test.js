95fba91f70114c931d74c1a1edacba24e89f4a65
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-10-08 19:04:56 -0300

    fix up test with compass' suggestion

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -46,7 +46,7 @@ describe("Application", () => {
   
   it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
     // 1. Render the Application.
-    const { container, debug } = render(<Application />);
+    const { container } = render(<Application />);
   
     // 2. Wait until the text "Archie Cohen" is displayed.
     await waitForElement(() => getByText(container, "Archie Cohen"));
@@ -59,21 +59,25 @@ describe("Application", () => {
     fireEvent.click(queryByAltText(appointment, "Delete"));
   
     // 4. Check that the confirmation message is shown.
-    expect(getByText(appointment, "Confirm")).toBeInTheDocument();
-
+    expect(
+      getByText(appointment, "Delete the appointment?")
+    ).toBeInTheDocument();
+  
     // 5. Click the "Confirm" button on the confirmation.
-    fireEvent.click(getByText(appointment, "Confirm"));
+    fireEvent.click(queryByText(appointment, "Confirm"));
+  
     // 6. Check that the element with the text "Deleting" is displayed.
     expect(getByText(appointment, "Deleting...")).toBeInTheDocument();
+  
     // 7. Wait until the element with the "Add" button is displayed.
     await waitForElement(() => getByAltText(appointment, "Add"));
+  
     // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
     const day = getAllByTestId(container, "day").find(day =>
       queryByText(day, "Monday")
     );
+  
     expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
-
-    debug();
   });
 
 });
