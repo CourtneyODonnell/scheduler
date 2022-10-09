ff8395014eba2d0f3389d32c59b5ea78f3ca6172
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-10-09 18:14:11 -0300

    add commented out code back for please select an interviewer error message

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -23,10 +23,10 @@ export default function Form(props) {
       setError("Student name cannot be blank");
       return;
     }
-    // if (interviewer === null) {
-    //   setError("Please select an interviewer");
-    //   return;
-    // }
+    if (interviewer === null) {
+      setError("Please select an interviewer");
+      return;
+    }
     setError("");
 
     props.onSave(student, interviewer);
