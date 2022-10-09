6d3b387e47d0f9c20d92dd5641fa4131114483a9
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-10-09 17:14:03 -0300

    use compass' edit test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -20,14 +20,17 @@ describe("Appointments", () => {
    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
    cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
-  it('should edit an interview', () => {
-    cy.get('[alt=Edit]').click({ force: true });
-    cy.get('[alt="Tori Malcolm"]').click();
-    cy.get('[data-testid=student-name-input]')
-      .clear()
-      .type('Zach');
-    cy.contains('Save').click();
-    cy.contains('.appointment__card--show', 'Zach');
-    cy.contains('.appointment__card--show', 'Tori Malcolm');
+  it("should edit an interview", () => {
+    cy.get("[alt=Edit]")
+      .first()
+      .click({ force: true });
+  
+    cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones");
+    cy.get("[alt='Tori Malcolm']").click();
+  
+    cy.contains("Save").click();
+  
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
   });
  });
